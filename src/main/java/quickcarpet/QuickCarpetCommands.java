41c527dcbe4fd3e331488bd81cac2cf2d416ffa8
Earthcomputer <burtonjae@hotmail.co.uk>
2018-09-22 04:31:17 +0100

    Register argument types

--- src/main/java/quickcarpet/QuickCarpetCommands.java
+++ src/main/java/quickcarpet/QuickCarpetCommands.java
@@ -3,6 +3,9 @@ package quickcarpet;
 import com.mojang.brigadier.CommandDispatcher;
 
 import net.minecraft.command.CommandSource;
+import net.minecraft.command.arguments.ArgumentSerializer;
+import net.minecraft.command.arguments.ArgumentTypes;
+import net.minecraft.util.ResourceLocation;
 
 public class QuickCarpetCommands {
 
@@ -11,6 +14,11 @@ public class QuickCarpetCommands {
         FillUpdatesCommand.register(dispatcher);
         TickCommand.register(dispatcher);
         StructureCommand.register(dispatcher);
+    }
+    
+    public static void registerCustomArgumentTypes() {
+        ArgumentTypes.register(new ResourceLocation("quickcarpet:template"), TemplateArgument.class, new ArgumentSerializer<>(TemplateArgument::template));
+        ArgumentTypes.register(new ResourceLocation("quickcarpet:long"), LongArgumentType.class, new ArgumentSerializer<>(LongArgumentType::longArg));
     }
     
 }
